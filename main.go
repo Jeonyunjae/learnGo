292261d05b0b4b86ffc25b417a57738ad7f91075
Jeonyunjae <55024678+Jeonyunjae@users.noreply.github.com>
2022-08-02 00:39:18 +0900

    1.8 Pointers

--- main.go
+++ main.go
@@ -1,56 +1,10 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
-func lenAndUpper(name string) (length int, uppercase string) {
-	length = len(name)
-	uppercase = strings.ToUpper(name)
-	return
-}
-
-func repeatMe(words ...string) {
-	fmt.Println(words)
-}
-
-func superAdd(numbers ...int) int {
-	total := 0
-	for _, number := range numbers {
-		total = total + number
-	}
-	return total
-}
-
-func canIDrink(age int) bool {
-
-	// 아래의 조건문과 같은 내용
-	// koreanAge := age + 2
-	// if koreanAge < 18
-
-	if koreanAge := age + 2; koreanAge < 18 {
-		return false
-	} else {
-		return true
-	}
-}
-
-func canIDrinkSwitch(age int) bool {
-	switch koreanAge := age + 2; koreanAge {
-	case 10:
-		return false
-	case 18:
-		return true
-	}
-	return false
-}
+import "fmt"
 
 func main() {
-	len, upper := lenAndUpper("nico")
-	repeatMe("nico", "lynn", "jyj")
-	fmt.Println(len, upper)
-
-	fmt.Println(superAdd(1, 2, 3, 4, 5))
-	fmt.Println(canIDrink(18))
+	a := 2
+	b := &a
+	*b = 20
+	fmt.Println(a, b)
 }
