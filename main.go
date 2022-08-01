88334eda76249b1b45e218860a7d92f0687a6b49
Jeonyunjae <55024678+Jeonyunjae@users.noreply.github.com>
2022-08-01 17:06:37 +0900

    1.5 for range, ...args

--- main.go
+++ main.go
@@ -15,8 +15,18 @@ func repeatMe(words ...string) {
 	fmt.Println(words)
 }
 
+func superAdd(numbers ...int) int {
+	total := 0
+	for _, number := range numbers {
+		total = total + number
+	}
+	return total
+}
+
 func main() {
 	len, upper := lenAndUpper("nico")
 	repeatMe("nico", "lynn", "jyj")
 	fmt.Println(len, upper)
+
+	fmt.Println(superAdd(1, 2, 3, 4, 5))
 }
