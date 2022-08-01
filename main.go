467af94f61b64613c806e81ce1b35890f085d46c
Jeonyunjae <55024678+Jeonyunjae@users.noreply.github.com>
2022-08-02 01:46:47 +0900

    1.11 structs

--- main.go
+++ main.go
@@ -2,21 +2,15 @@ package main
 
 import "fmt"
 
+type person struct {
+	name    string
+	age     int
+	favFood []string
+}
+
 func main() {
-	//슬라이스 갯수 제한이 필요없을때
-	namesSlice := []string{"jyj", "kjh", "jjs", "jds"}
-	namesSlice = append(namesSlice, "jdh")
-	fmt.Println("Slice", namesSlice)
-
-	//배열
-	names := [5]string{"jyj", "kjh", "jjs", "jds"}
-	fmt.Println("Array", names)
-
-	//map
-	nico := map[string]string{"name": "nico", "age": "12"}
-	fmt.Println("Map", nico)
-
-	for key, value := range nico {
-		fmt.Println(key, value)
-	}
+	favFood := []string{"김치", "라면"}
+	//nico := person{"nico", 18, favFood}
+	nico := person{name: "nico", age: 18, favFood: favFood}
+	fmt.Println(nico)
 }
