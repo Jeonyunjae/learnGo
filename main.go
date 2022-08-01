069c9e68107ee37557ceb85e06c54fc9cceaeb8f
Jeonyunjae <55024678+Jeonyunjae@users.noreply.github.com>
2022-08-02 00:31:22 +0900

    1.7 switch

--- main.go
+++ main.go
@@ -23,10 +23,34 @@ func superAdd(numbers ...int) int {
 	return total
 }
 
+func canIDrink(age int) bool {
+
+	// 아래의 조건문과 같은 내용
+	// koreanAge := age + 2
+	// if koreanAge < 18
+
+	if koreanAge := age + 2; koreanAge < 18 {
+		return false
+	} else {
+		return true
+	}
+}
+
+func canIDrinkSwitch(age int) bool {
+	switch koreanAge := age + 2; koreanAge {
+	case 10:
+		return false
+	case 18:
+		return true
+	}
+	return false
+}
+
 func main() {
 	len, upper := lenAndUpper("nico")
 	repeatMe("nico", "lynn", "jyj")
 	fmt.Println(len, upper)
 
 	fmt.Println(superAdd(1, 2, 3, 4, 5))
+	fmt.Println(canIDrink(18))
 }
