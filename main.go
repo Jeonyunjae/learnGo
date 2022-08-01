9930d433055d1e8abeba051bb28017d7fad653a6
Jeonyunjae <55024678+Jeonyunjae@users.noreply.github.com>
2022-08-02 00:56:05 +0900

    1.10 Maps

--- main.go
+++ main.go
@@ -6,8 +6,17 @@ func main() {
 	//슬라이스 갯수 제한이 필요없을때
 	namesSlice := []string{"jyj", "kjh", "jjs", "jds"}
 	namesSlice = append(namesSlice, "jdh")
-	fmt.Println(namesSlice)
+	fmt.Println("Slice", namesSlice)
 
+	//배열
 	names := [5]string{"jyj", "kjh", "jjs", "jds"}
-	fmt.Println(names)
+	fmt.Println("Array", names)
+
+	//map
+	nico := map[string]string{"name": "nico", "age": "12"}
+	fmt.Println("Map", nico)
+
+	for key, value := range nico {
+		fmt.Println(key, value)
+	}
 }
